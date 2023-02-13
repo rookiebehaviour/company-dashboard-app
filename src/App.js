852b3f3bffc26ac277d7d37b9c82777a2b4f604b
Jacqueline M <75797321+rookiebehaviour@users.noreply.github.com>
2023-02-13 17:38:56 -0500

    Completed theme and added topbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
+import { colorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Dashboard from "./scenes/dashboard";
+
 function App() {
-  return <div className="app"></div>;
+  const [theme, colorMode] = useMode();
+
+  return (
+    <colorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
+          </main>
+        </div>
+        ;
+      </ThemeProvider>
+    </colorModeContext.Provider>
+  );
 }
 
 export default App;
