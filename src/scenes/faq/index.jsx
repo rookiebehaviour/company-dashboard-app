517bb7f44627f9877e9da8fd8bb4203b7f830b3c
Jacqueline M <75797321+rookiebehaviour@users.noreply.github.com>
2023-02-15 03:54:24 -0500

    Finished FAQ page

--- src/scenes/faq/index.jsx
+++ src/scenes/faq/index.jsx
@@ -13,6 +13,42 @@ const FAQ = () => {
   return (
     <Box m="20px">
       <Header title="FAQ" subtitle="Frequently asked questions" />
+
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            How many years have you been coding?
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>A little over 2 years now.</Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            What are your favorite things to build?
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Websites for the creativity, but I love dashboards and have an
+            interest in building end user and developer tools.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+
+      <Accordion>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography color={colors.greenAccent[500]} variant="h5">
+            Another great question
+          </Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>And another great answer.</Typography>
+        </AccordionDetails>
+      </Accordion>
     </Box>
   );
 };
